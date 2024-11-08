0fde184edce229481852bbfc1dae284114e24c13
Puru <purusho1428@gmail.com>
2024-11-08 18:00:14 +0530

    Index Page modification

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -10,9 +10,9 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           {/* Brand */}
-          <Link to="/" className="lg:text-2xl sm:text-xl font-bold">
+          <h1 className="lg:text-2xl sm:text-xl font-bold">
             Game Challenger
-          </Link>
+          </h1>
           {/* Desktop Menu */}
           <div className="hidden md:flex space-x-6">
             {/* <Link
